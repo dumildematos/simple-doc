cb5bf205cd3f0dacf5e8fda73e9e789a2cc51043
Dumilde Matos <dumildejosematos@gmail.com>
2021-09-27 16:02:33 +0100

    Add login form

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,54 +1,22 @@
 import React from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.global.css';
+import Home from './Home/Home';
 import Login from './Login/Login';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
+const hasValidToken = true;
 
 export default function App() {
+  if (!hasValidToken) {
+    return <Login />;
+  }
   return (
     <Router>
       {/* <Switch>
-        <Route path="/" component={Hello} />
-      </Switch> */}
+          <Route path="/" component={Hello} />
+        </Switch> */}
       <Switch>
-        <Route path="/" component={Login} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
--- src/renderer/Login/Login.tsx
+++ src/renderer/Login/Login.tsx
@@ -1,16 +1,82 @@
-import React from 'react';
-import { Layout, Carousel, Row, Col } from 'antd';
+import React, { useState } from 'react';
+import {
+  Layout,
+  Carousel,
+  Row,
+  Col,
+  Form,
+  Button,
+  Input,
+  Checkbox,
+} from 'antd';
 import styled from '@xstyled/styled-components';
+import { FaLinkedinIn } from '@react-icons/all-files/fa/FaLinkedinIn';
+import { FaFacebookF } from '@react-icons/all-files/fa/FaFacebookF';
+import { FaGoogle } from '@react-icons/all-files/fa/FaGoogle';
+import { useHistory, Redirect } from 'react-router-dom';
 import folder1 from './undraw_Add_notes_re_ln36.svg';
 
-const { Content, Footer } = Layout;
+const { Content } = Layout;
+
+const LoginBox = styled.divBox`
+  margin-top: 15%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-container {
+    height: 100%;
+    .login-logo {
+      header {
+        h1 {
+          font-size: 2rem;
+          color: var(--black-1);
+          font-family: var(--BerlinRegular);
+          b {
+            font-family: var(--BerninDemiBold);
+            color: var(--purple-1);
+          }
+        }
+      }
+      p {
+        color: var(--black-1);
+        font-family: var(--candara);
+        font-size: 1rem;
+      }
+    }
+    .btnLogin {
+      background: linear-gradient(#6474e4, #4c5fe1) !important;
+      border: none;
+    }
+    .social-login {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      div {
+        button {
+          margin: 5px;
+          border: none;
+          color: #F7F7F7;
+          &:nth-child(1) {
+            background-color: #0a66c2;
+          }
+          &:nth-child(2) {
+            background-color: #395693;
+          }
+          &:nth-child(3) {
+            background-color: #dd4b39;
+          }
+        }
+      }
+    }
+  }
+`;
 
 const FolderSlider = styled.divBox`
-  /* background-color: red; */
   height: 100vh;
   position: absolute;
   width: 100%;
   overflow: hidden;
+
   div.folder {
     position: absolute;
     right: -15%;
@@ -55,17 +121,44 @@ const FolderSlider = styled.divBox`
       height: 100%;
       color: #fff;
       background: transparent;
+      h3 {
+        font-weight: bolder;
+        font-family: var(--candara);
+        color: white;
+        font-size: 2rem;
+      }
       p {
         font-size: 12px;
+        font-family: var(--candara);
+        width: 80%;
       }
       img {
         margin: auto;
+        width: 150px;
       }
     }
   }
 `;
 
-export default function Login() {
+const FooterBox = styled.footerBox`
+  background: var(--white-1);
+`;
+
+type RequiredMark = boolean | 'optional';
+
+export default function Login(props: any) {
+  const [form] = Form.useForm();
+  const history = useHistory();
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+    return <Redirect push to="/" />;
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <>
       <Layout className="layout">
@@ -73,7 +166,107 @@ export default function Login() {
           <div className="site-layout-content style={{ height: '100%' }}">
             <Row style={{ height: '100%' }}>
               <Col span={12} style={{ height: '100%' }}>
-                col-12
+                <LoginBox>
+                  <div className="login-container">
+                    <Row>
+                      <Col span={24}>
+                        <div className="login-logo">
+                          <header>
+                            <h1>
+                              Simple <b>Doc</b>
+                            </h1>
+                          </header>
+                          <p>
+                            Documente seus produtos, serviços, processos e muito
+                            de forma fácil e ágil.
+                          </p>
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col span={16}>
+                        <Form
+                          layout="vertical"
+                          initialValues={{ remember: true }}
+                          onFinish={onFinish}
+                          onFinishFailed={onFinishFailed}
+                          autoComplete="off"
+                        >
+                          <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[
+                              {
+                                required: true,
+                                message: 'Please input your username!',
+                              },
+                            ]}
+                          >
+                            <Input />
+                          </Form.Item>
+                          <Form.Item
+                            label="Palavra-passe:"
+                            name="password"
+                            rules={[
+                              {
+                                required: true,
+                                message: 'Please input your password!',
+                              },
+                            ]}
+                          >
+                            <Input.Password />
+                          </Form.Item>
+                          <Form.Item name="remember" valuePropName="checked">
+                            <Checkbox>Remember me</Checkbox>
+                          </Form.Item>
+
+                          <Form.Item>
+                            <Button
+                              className="btnLogin"
+                              type="primary"
+                              block
+                              htmlType="submit"
+                            >
+                              Entrar
+                            </Button>
+                          </Form.Item>
+                        </Form>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col span={16} style={{ textAlign: 'center' }}>
+                        <p>or</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="social-login" span={16}>
+                        <div>
+                          <Button>
+                            <FaLinkedinIn />
+                          </Button>{' '}
+                          <Button>
+                            <FaFacebookF />
+                          </Button>{' '}
+                          <Button>
+                            <FaGoogle />
+                          </Button>
+                        </div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col
+                        span={16}
+                        className="p-10 sign-up"
+                        style={{ marginTop: '1rem' }}
+                      >
+                        <span>
+                          Ainda não tem uma conta,
+                          <Button type="link">Registra-se</Button>
+                        </span>
+                      </Col>
+                    </Row>
+                  </div>
+                </LoginBox>
               </Col>
               <Col span={12} style={{ height: '100%' }}>
                 <FolderSlider>
@@ -111,9 +304,9 @@ export default function Login() {
             </Row>
           </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
+        <FooterBox style={{ textAlign: 'center' }}>
           Ant Design ©2018 Created by Ant UED
-        </Footer>
+        </FooterBox>
       </Layout>
     </>
   );
